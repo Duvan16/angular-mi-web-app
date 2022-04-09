2f062cb1aa6d9f2d3892054bd4ce3f3b2c29441a
Duvan16 <duvan4448@gmail.com>
2022-04-08 22:43:26 -0500

    1-ComunicacionEntreComponentes

---
+++ src/app/Libros/libros.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libros',
+  templateUrl: './libros.component.html',
+})
+export class LibrosComponent {
+  libros = ['Matematica', 'Algrebra', 'Algoritmos'];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario.component';
 import { FormsModule } from '@angular/forms';
+import { LibrosComponent } from './Libros/libros.component';
+import { LibroComponent } from './Libro/libro.component';
 
 @NgModule({
-  declarations: [AppComponent, UsuarioComponent],
+  declarations: [
+    AppComponent,
+    UsuarioComponent,
+    LibrosComponent,
+    LibroComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
