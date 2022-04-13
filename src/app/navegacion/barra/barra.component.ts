51ce25ed40563cadfb2fd7ae8aba1ae8878b046f
Duvan16 <duvan4448@gmail.com>
2022-04-12 22:34:47 -0500

    15-IniciarSesión

--- src/app/navegacion/barra/barra.component.ts
+++ src/app/navegacion/barra/barra.component.ts
@@ -1,18 +1,37 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  OnDestroy,
+} from '@angular/core';
+import { SeguridadService } from '../../seguridad/seguridad.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-barra',
   templateUrl: './barra.component.html',
   styleUrls: ['./barra.component.css'],
 })
-export class BarraComponent implements OnInit {
+export class BarraComponent implements OnInit, OnDestroy {
   @Output() menuToggle = new EventEmitter<void>();
+  estadoUsuario?: boolean;
+  usuarioSubscription?: Subscription;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(private seguridadServicio: SeguridadService) {}
+  ngOnInit(): void {
+    this.usuarioSubscription = this.seguridadServicio.seguridadCambio.subscribe(
+      (status) => {
+        this.estadoUsuario = status;
+      }
+    );
+  }
 
   onMenuToggleDispatch() {
     this.menuToggle.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.usuarioSubscription?.unsubscribe();
   }
 }
