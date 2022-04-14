1391d40d795965b0b297cd8ecfd93439721df303
Duvan16 <duvan4448@gmail.com>
2022-04-13 21:10:50 -0500

    Agregar Select - Combobox

--- src/app/books/book-nuevo.component.ts
+++ src/app/books/book-nuevo.component.ts
@@ -1,25 +1,34 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatOption } from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
+import { Autor } from '../autores/autor.model';
+import { AutoresService } from '../autores/autores.service';
 import { BooksService } from './books.service';
 
 @Component({
   selector: 'app-book-nuevo',
   templateUrl: 'book-nuevo.component.html',
 })
-export class BookNuevoComponent {
+export class BookNuevoComponent implements OnInit {
   selectAutor!: string;
   selectAutorTexto!: string;
   fechaPublicacion!: string;
   @ViewChild(MatDatepicker) picker!: MatDatepicker<Date>;
 
+  autores: Autor[] = [];
+
   constructor(
     private bookService: BooksService,
-    private dialogRef: MatDialog
+    private dialogRef: MatDialog,
+    private autoresService: AutoresService
   ) {}
+
+  ngOnInit(): void {
+    this.autores = this.autoresService.obtenerAutores();
+  }
 
   selected(event: MatSelectChange) {
     this.selectAutorTexto = (event.source.selected as MatOption).viewValue;
